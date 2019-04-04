dbba0dafe64e3a0df335d85165221b1cb7241781
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2019-04-04 15:40:55 -0300

    add dao class

---
+++ src/class/dao/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DAO;
+
+use PDOException;
+use Connection\Database;
+
+class Login {
+
+  public static function verify($user, $password){
+    $query = "SELECT U_ID, U_NAME FROM USERS 
+                  WHERE U_LOGIN=? AND U_PASSWORD=?";
+    
+    return Database::select([$user, $password], $query);    
+  }
+  
+}
