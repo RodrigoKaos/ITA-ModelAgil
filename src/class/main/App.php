e30a2083789432d7bea9b08b94212e0c6f3560b3
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2019-04-04 15:40:27 -0300

    add dao class

--- src/class/main/App.php
+++ src/class/main/App.php
@@ -3,63 +3,36 @@
 namespace Main;
 
 use PDO;
+use DAO\Login;
 use Connection\Database;
 
 class App{
     
-    private $dbConnection;
+  private $dbConnection;
 
-    public function __construct(){
-        // include('config.php');
-        $this->dbConnection = Database::getConnection();
+  public function __construct(){
+    $this->dbConnection = Database::getConnection();
+  }
+
+  public function login(){
+    if( !empty($_POST) ){
+      if( isset($_POST['login'])  && isset($_POST['password']) ) {
+        $result = Login::verify($_POST['login'], $_POST['password']);
+
+        if( !$result ){
+          $err_msg = 'Invalid username or password...';
+          $_SESSION['LOGERROR'] = $err_msg;
+        }else{
+          $_SESSION['UID'] = $result->U_ID;
+          $_SESSION['UNAME'] = $result->U_NAME;
+        }        
+      }
     }
+  }
 
-    private function loginHelper($username, $password){
-        try{
-            $loginQuery = 'SELECT 
-                            U_ID, U_NAME FROM USERS 
-                        WHERE 
-                            U_LOGIN=:login AND U_PASSWORD=:password';
-
-            $query = $this->dbConnection->prepare( $loginQuery );
-            $query->bindParam("login", $username, PDO::PARAM_STR);
-            $query->bindParam("password", $password, PDO::PARAM_STR);
-            $query->execute();
-
-            if( $query->rowCount() > 0 )
-                return $query->fetch( PDO::FETCH_OBJ );
-        
-        }catch( PDOException $e ){
-            print "Error: " . $e->getMessage() . "<br/>";
-        }
-
-        return false;
-    }
-
-    public function login(){
-        if(! empty( $_POST ) ){    
-            if(
-                isset( $_POST['login'] )  && 
-                isset( $_POST['password'] ) 
-            ){
-                $result = $this->loginHelper(
-                                    $_POST['login'], 
-                                    $_POST['password'] );
-                
-                if( ! $result ){
-                    $err_msg = 'Invalid username or password...';
-                    $_SESSION['LOGERROR'] = $err_msg;
-                }else{
-                    $_SESSION['UID'] = $result->U_ID;
-                    $_SESSION['UNAME'] = $result->U_NAME;
-                }        
-            }
-        }
-    }
-
-    private function isLogged(){
-        return isset($_SESSION['UID']);
-    }
+  private function isLogged(){
+      return isset($_SESSION['UID']);
+  }
 
     public function createView(){
         $str_view = '_view/pages/login.php';
