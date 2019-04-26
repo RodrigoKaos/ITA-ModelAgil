2dd417f7163ddcc7a1d976d77d62c213aa4372ad
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2019-04-26 16:01:42 -0300

    added mount template function

--- app/class/view/Renderer.php
+++ app/class/view/Renderer.php
@@ -6,34 +6,40 @@ class Renderer {
     
   private $templateFile;
 
-  public function __construct($template = "app/pages/404.php", $data = null) {
-    if(file_exists($template)){
-      $this->load($template);
-      $this->ParseData($data);
-      $this->render();
-    }else{
-      echo "Not Found";
+  public function __construct($template, $data = null) {
+    if(!file_exists($template)){
+      $template = 'app/pages/404.php';
     }
+    
+    $this->mountTemplate($template);
+    $this->ParseData($data);
+    $this->render();
   }
 
   private function load($templatePath) {
-    $templateFile = fopen($templatePath, "r");
-    var_dump($templateFile);
-    if($templateFile != null){
-      $this->templateFile = fread($templateFile, filesize($templatePath));
-    }
+    return file_get_contents($templatePath); 
   }
 
   private function parseData($args) {
     if(args != null){
       foreach ($args as $key => $value) {
+        echo $key . " - " . $value . "<br>";
         $this->templateFile = str_replace("{".$key."}", $value, $this->templateFile); 
       }
     }  
   }
 
   private function render() {
-    var_dump( $this->templateFile );
+    eval( "?>" . $this->templateFile);
+  }
+    
+  private function mountTemplate($templatePath){
+    
+    $headerTemplate = $this->load('app/pages/header.test.php');
+    $contentTemplate = $this->load($templatePath);
+    $footerTemplate = $this->load('app/pages/footer.php');
+    
+    $this->templateFile = $headerTemplate . $contentTemplate . $footerTemplate;
   }
 
 }
